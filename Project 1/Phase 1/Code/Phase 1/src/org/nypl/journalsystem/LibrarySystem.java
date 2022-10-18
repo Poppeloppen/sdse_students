a079fe5c4e1bc976fe00fc875e04dbc4649901a7
Poppeloppen <victor.popp.henriksen@gmail.com>
2022-10-19 00:02:49 +0200

    initialized first 5 journals

--- Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/LibrarySystem.java
+++ Project 1/Phase 1/Code/Phase 1/src/org/nypl/journalsystem/LibrarySystem.java
@@ -17,17 +17,20 @@ import org.apache.commons.csv.CSVRecord;
 
 public class LibrarySystem {
 	
-	List<Journal> journals;
+	Map<String, Journal> journals;
 	Map<Integer, String> IDAuthorMap;
 	
 	
 	
 	public LibrarySystem() {
-		//TODO: Initialize system with default journals.
-		journals = new ArrayList<Journal>();
+		//Initialize system with default journals.
+		journals = new HashMap<String, Journal>();
 		IDAuthorMap = new HashMap<Integer, String>();
-		journals.add(new Journal("Higher Education", "Springer", "Germany", "0018-1560"));
-		
+		journals.put("0018-1560", new Journal("Higher Education", "Springer", "Germany", "0018-1560"));
+		journals.put("0346-2511", new Journal("System", "Elsevier", "Netherlands", "0346-2511"));
+		journals.put("2451-9294", new Journal("Chem", "Elsevier", "Netherlands", "2451-9294"));
+		journals.put("1476-4687", new Journal("Nature", "Nature Research", "Great Britain", "1476-4687"));
+		journals.put("0147-2011", new Journal("Society", "Springer", "Germany", "0147-2011"));
 	}
 	
 	public void load() throws FileNotFoundException, IOException {
@@ -53,7 +56,7 @@ public class LibrarySystem {
 		}
 		
 		//System.out.println(IDAuthorMap);
-		System.out.println(journals.get(0));
+		//System.out.println(journals.get("0018-1560"));
 	}
 	
 	protected void loadArticles() throws FileNotFoundException, IOException {
